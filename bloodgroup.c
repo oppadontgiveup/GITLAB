53402d6ab086362d36d520771d525bbc5e3d9c8c
helloh0ng <pimlapas.hong@mail.kmutt.ac.th>
2021-09-16 21:26:51 +0700

    Corrected Merge Conflict of bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,11 +5,11 @@ void main()
   int i;
   do
   {
-    printf("What is hong Name? B ");
+    printf("A What is hong Name? B ");
     scanf("%s", Name);
     do
     {
-      printf("What is your hong blood groups B [A, B, AB, or O]? ");
+      printf("A What is your hong blood groups B [A, B, AB, or O]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
